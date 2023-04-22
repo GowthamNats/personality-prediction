cb7288f4192169239fcfd3170d828d9430a8bb91
GowthamNats <72858459+GowthamNats@users.noreply.github.com>
2023-04-22 17:13:16 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/home';
+import Questionnaire from './Pages/questionnaire';
+import Result from './Pages/result';
 
 function App() {
+
+  const [result, setResult] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />}></Route>
+        <Route path="questionnaire" element={<Questionnaire setResult={setResult} />} ></Route>
+        <Route path="result" element={<Result result={result} />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
